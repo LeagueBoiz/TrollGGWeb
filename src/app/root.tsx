4b412ca99e3e55be3e6d36da19452f3eb244a694
reality023 <reality023@naver.com>
2023-01-21 15:49:19 +0900

    chore: redux 기본 설정

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import GlobalStyle from "@/styles/GlobalStyle";
 import React from "react";
+import store from "@/redux/store";
+import { Provider } from "react-redux";
 
 const RootComponent: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 	return (
 		<>
 			<GlobalStyle />
-			{children}
+			<Provider store={store}>{children}</Provider>
 		</>
 	);
 };
