10c65e1338a556f4559e2d2ca4fedc476a884931
reality023 <reality023@naver.com>
2023-01-21 17:48:21 +0900

    chore: 프리티어, tailwind 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,60 @@
-const px0to10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) };
-const px0to100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
-const px0to200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
-const px0to1000 = { ...Array.from(Array(1001)).map((_, i) => `${i}px`) };
-const px0to2000 = { ...Array.from(Array(2001)).map((_, i) => `${i}px`) };
+const pxrem0to10 = { ...Array.from(Array(11)).map((_, i) => `${i * 0.0625}rem`) };
+const pxrem0to100 = { ...Array.from(Array(101)).map((_, i) => `${i * 0.0625}rem`) };
+const pxrem0to200 = { ...Array.from(Array(201)).map((_, i) => `${i * 0.0625}rem`) };
+const pxrem0to1000 = { ...Array.from(Array(1001)).map((_, i) => `${i * 0.0625}rem`) };
+const pxrem0to2000 = { ...Array.from(Array(2001)).map((_, i) => `${i * 0.0625}rem`) };
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	purge: ["./src/**/*.{js,ts,jsx,tsx}"],
-	content: [],
-	theme: {
-		extend: {
-			borderWidth: px0to10,
-			fontSize: px0to100,
-			lineHeight: px0to100,
-			minWidth: px0to200,
-			minHeight: px0to200,
-			spacing: px0to200,
-			width: px0to2000,
-			height: px0to2000,
-			left: px0to1000,
-			right: px0to1000,
-			top: px0to1000,
-			bottom: px0to1000,
-			borderRadius: px0to100,
-		},
-	},
-	plugins: [],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {
+      borderWidth: pxrem0to10,
+      fontSize: pxrem0to100,
+      lineHeight: pxrem0to100,
+      minWidth: pxrem0to200,
+      minHeight: pxrem0to200,
+      spacing: pxrem0to200,
+      width: pxrem0to2000,
+      height: pxrem0to2000,
+      left: pxrem0to1000,
+      right: pxrem0to1000,
+      top: pxrem0to1000,
+      bottom: pxrem0to1000,
+      borderRadius: pxrem0to100,
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      font: {
+        default: '#333333',
+      },
+      white: '#ffffff',
+      black: '#000000',
+      grey: {
+        default: '#111111',
+        100: '#333333',
+        200: '#666666',
+        300: '#999999',
+        400: '#aaaaaa',
+        500: '#cccccc',
+        600: '#dddddd',
+        700: '#eeeeee',
+        800: '#f5f5f5',
+        900: '#fafafa',
+      },
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      phone: { max: '480px' },
+      tablet: { min: '481px', max: '768px' },
+      desktop: { min: '769px' },
+    },
+  },
+  plugins: [],
 };
